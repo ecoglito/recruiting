8e9742d33eb78e45778e65b75f7a17d32b4c13b3
ecoglito <ecoglito@usc.edu>
2023-05-22 12:26:49 -0700

    remove github login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -15,10 +15,6 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
   ],
   secret:process.env.NEXTAUTH_SECRET,
 };
